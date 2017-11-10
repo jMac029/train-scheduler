da209a403cf746c4e8cf02be17a975a32bee6348
James McMillan <jmcmillan29@gmail.com>
2017-11-10 13:16:23 -0800

    added functionality to capture the form field, added some extra colors

--- assets/javascript/controller.js
+++ assets/javascript/controller.js
@@ -1,2 +1,33 @@
  // This is where all the javascript will
  // live for the project that connects the view and model
+
+// controller object
+ let controller = {
+
+ 	// capture all the fields in the form area
+ 	captureFormFields: () => {
+ 		$('body').on("click", ".button-add", () => {
+ 			// prevent form from submitting
+      		event.preventDefault();
+
+      		// variables from the form field values
+ 			trainNumber = $('#train-number').val().trim();
+ 			trainLine = $('#train-line').val().trim();
+ 			trainDestination = $('#train-destination').val().trim();
+ 			trainDeparture = $('#train-departure').val().trim();
+ 			trainFrequency = $('#train-frequency').val().trim();
+ 			trainPlatform = $('#train-platform').val().trim();
+
+ 			console.log(trainNumber)
+ 			console.log(trainLine)
+ 			console.log(trainDestination)
+ 			console.log(trainDeparture)
+ 			console.log(trainFrequency)
+ 			console.log(trainPlatform)
+
+ 			// clear all the fields in the form
+ 			$('.form-control').val("");
+
+ 		})
+ 	}
+ }
--- assets/javascript/model.js
+++ assets/javascript/model.js
@@ -1,2 +1,24 @@
  // This is where all the data and global variables will 
  // live for the project
+
+ // Initialize Firebase
+var config = {
+apiKey: "AIzaSyApIvO1MNIvE_WNeyS7jFrDHuI4BQ56rFE",
+authDomain: "train-scheduler-f365b.firebaseapp.com",
+databaseURL: "https://train-scheduler-f365b.firebaseio.com",
+projectId: "train-scheduler-f365b",
+storageBucket: "",
+messagingSenderId: "471705960814"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// Form Variables to be passed between objects
+var trainNumber;
+var trainLine;
+var trainDestination;
+var trainDeparture;
+var trainFrequency;
+var trainPlatform;
+
