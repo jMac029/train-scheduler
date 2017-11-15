a99b3db662ca4986fdbaae2a04a13b95a87a8f84
James McMillan <jmcmillan29@gmail.com>
2017-11-14 22:38:59 -0800

    fixed a bug with displaying the time

--- assets/javascript/view.js
+++ assets/javascript/view.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
 	controller.captureFormFields();
 	model.initialDatabasePull();
 	setInterval(function() {model.initialDatabasePull()}, 60000);
+	view.updateCurrentTime();
 	setInterval(function() {view.updateCurrentTime()}, 1000);
 
 });
